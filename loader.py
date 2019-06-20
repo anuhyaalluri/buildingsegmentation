2ac94315375025fd47ae10981cdbf78a86f75e3c
anuhyaalluri <anuhya.alluri@gmail.com>
2019-06-20 10:54:47 +0530

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,6 +1,3 @@
-## Starts new threads for loading data into the memory.
-## "processor" function passed as an argument to constructor
-## is responsible for reading and preprocessing the data.
 
 from queue import Queue, Full
 import random
